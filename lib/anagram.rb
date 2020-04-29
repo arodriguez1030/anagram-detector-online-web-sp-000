024e4aece754dd6714654b4f1427e837e3795a57
Ashley Rodriguez <arodriguez1030@gmail.com>
2020-04-29 23:02:25 +0000

    Automatically backed up by Learn

--- lib/anagram.rb
+++ lib/anagram.rb
@@ -1,1 +1,13 @@
 # Your code goes here!
+class Anagram
+attr_accessor :words
+ def initialize(words)
+   @words = words
+ end
+ 
+   def match(word_array)
+    word_array.select do |word|
+      @words.split("").sort
+    end
+  end
+end
